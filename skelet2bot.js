c19168b38da2281e70cfd5fb55f24eefc43f6120
MilkyPL <kubex.jb@gmail.com>
2017-08-12 21:30:56 +0200

    migrate to botgram and add some snazzy functions

--- skelet2bot.js
+++ skelet2bot.js
@@ -1,8 +1,19 @@
-const Telegraf = require('telegraf')
+var botgram = require("botgram");
+var bot = botgram(process.argv[2]);
 
-const app = new Telegraf(process.env.BOT_TOKEN)
-app.command('start', ({ from, reply }) => {
-  console.log('start', from)
-  return reply('fuck off')
-})
-app.startPolling()
+var rant = "When you say Linux, you probably mean the GNU operating " +
+"system, running Linux as the kernel. You should therefore say " +
+"GNU/Linux or GNU+Linux.";
+var fag = "When you say Raphy, you probably mean the faggot " +
+", running Raphy as the kernel. You should therefore say " +
+"faggot/Raphy or faggot+Raphy.";
+
+bot.text(function (msg, reply, next) {
+  var text = msg.text.toLowerCase();
+  if (text.indexOf("raphy") != -1 && text.indexOf("faggot") == -1)
+    reply.reply(msg).text(fag);
+});
+
+bot.command("start", function (msg, reply, next) {
+  reply.text("fuck off");
+});
