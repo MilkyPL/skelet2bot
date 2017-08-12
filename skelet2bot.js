aa5021847390bb90770b0729d962275ee76f85e8
MilkyPL <kubex.jb@gmail.com>
2017-08-12 17:13:50 +0200

    rewrite again

--- skelet2bot.js
+++ skelet2bot.js
@@ -1,21 +1,8 @@
-'use strict';
+const Telegraf = require('telegraf')
 
-const Telegraf = require('telegraf');
-
-const fs = require('fs');
-
-const config = require('./config');
-
-const app = new Telegraf(config.telegram.token);
-
-app.telegram.getMe().then(bot =>
-	app.options.username = bot.username);
-
-fs.readdirSync('commands')
-	.map(x => ({
-		file: './commands/' + x,
-		name: x.split('.').slice(0, -1).join('.')
-	})).forEach(command =>
-		app.command(command.name, require(command.file)));
-
-app.startPolling();
+const app = new Telegraf(process.env.BOT_TOKEN)
+app.command('start', ({ from, reply }) => {
+  console.log('start', from)
+  return reply('fuck off')
+})
+app.startPolling()
