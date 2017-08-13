9b290196fde08f048f942db7499107ae7979a719
MilkyPL <kubex.jb@gmail.com>
2017-08-13 20:05:46 +0200

    add new unfinished commands

--- skelet2bot.js
+++ skelet2bot.js
@@ -1,6 +1,7 @@
 var botgram = require("botgram");
 var bot = botgram(process.argv[2]);
 
+var feature = "This feature is either under construction or I'm too retarded to implement it";
 var rant = "When you say Linux, you probably mean the GNU operating " +
 "system, running Linux as the kernel. You should therefore say " +
 "GNU/Linux or GNU+Linux.";
@@ -21,3 +22,10 @@ bot.command("start", function (msg, reply, next) {
 	reply.text("fuck off");
 });
 
+bot.command("price", function (msg, reply, next) {
+	reply.text(feature);
+});
+
+bot.command("weather", function (msg, reply, next) {
+	reply.text(feature);
+});
