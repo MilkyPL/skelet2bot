7be5710069fbdb7e23b540283a76803d7c70dba0
MilkyPL <kubex.jb@gmail.com>
2017-08-14 22:19:25 +0200

    ass

--- skelet2bot.js
+++ skelet2bot.js
@@ -1,23 +1,30 @@
-var botgram = require("botgram");
-var bot = botgram(process.argv[2]);
+const botgram = require("botgram");
+const { json } = require('req');
+const bot = botgram(process.argv[2]);
 const rants = require('./rants.json');
+const feature = "This feature is either under construction or i'm too retarded to implement it"
 
 bot.text(function (msg, reply, next) {
-	var text = msg.text.toLowerCase();
-	if (text.includes("linux") && !text.includes("gnu"))
-		reply.reply(msg).text(rants.linux[Math.floor(Math.random()*rants.linux.length)]);
-	if (text.includes("raphy") && !text.includes("faggot"))
-		reply.reply(msg).text(rants.raphy[Math.floor(Math.random()*rants.raphy.length)]);
+    const text = msg.text.toLowerCase();
+    if (text.includes("linux") && !text.includes("gnu"))
+		    reply.reply(msg).text(rants.linux[Math.floor(Math.random()*rants.linux.length)]);
+    if (text.includes("raphy") && !text.includes("faggot"))
+		    reply.reply(msg).text(rants.raphy[Math.floor(Math.random()*rants.raphy.length)]);
 });
 
 bot.command("start", function (msg, reply, next) {
-	reply.text("fuck off");
+    reply.text("fuck off");
 });
 
 bot.command("price", function (msg, reply, next) {
-	reply.text(feature);
+    const coin = msg.args(2)
+    json('https://api.coinmarketcap.com/v1/ticker/')
+        .then(data =>
+              data.forEach(obj =>
+                           reply.text(obj.name.filter(includes(coin)) +
+                                      ': ' + obj.price_usd)));
 });
 
 bot.command("weather", function (msg, reply, next) {
-	reply.text(feature);
+    reply.text(feature);
 });
--- utils.js
+++
@@ -1,5 +0,0 @@
-const commandText = msg => {
-	  let text = msg.args(2);
-	  text = text.join(' ');
-	  return text;
-};
