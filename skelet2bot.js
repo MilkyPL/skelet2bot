c44af30e62012adc67afaeae0c5fd3fc8f539707
MilkyPL <kubex.jb@gmail.com>
2017-08-14 23:12:40 +0200

    price command finally done thanks to @trgwii and bd

--- skelet2bot.js
+++ skelet2bot.js
@@ -2,7 +2,7 @@ const botgram = require("botgram");
 const { json } = require('req');
 const bot = botgram(process.argv[2]);
 const rants = require('./rants.json');
-const feature = "This feature is either under construction or i'm too retarded to implement it"
+const feature = "This feature is either under construction or i'm too retarded to implement it";
 
 bot.text(function (msg, reply, next) {
     const text = msg.text.toLowerCase();
@@ -17,12 +17,14 @@ bot.command("start", function (msg, reply, next) {
 });
 
 bot.command("price", function (msg, reply, next) {
-    const coin = msg.args(2)
+    const coin = msg.args();
+    let crap;
     json('https://api.coinmarketcap.com/v1/ticker/')
-        .then(data =>
-              data.forEach(obj =>
-                           reply.text(obj.name.filter(includes(coin)) +
-                                      ': ' + obj.price_usd)));
+        .then(data => crap = data)
+        .then(()=> {
+            let balls = crap.find(obj => obj.symbol === coin.toUpperCase());
+            reply.text(balls.name + ': ' + balls.price_usd);
+        });
 });
 
 bot.command("weather", function (msg, reply, next) {
