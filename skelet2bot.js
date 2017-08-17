bc5bc17cb251c99528659e1a7394664a18dd7d05
Thomas Rory Gummerson <thomas@gummerson.no>
2017-08-17 15:18:46 +0200

    Un-mooed and other shortened

--- skelet2bot.js
+++ skelet2bot.js
@@ -9,6 +9,7 @@ const cowsay = require("cowsay");
 const args = text => text.split(" ").slice(1);
 
 const bot = new Telegraf(process.argv[2]);
+
 const feature = "This feature is either under construction or i'm too retarded to implement it";
 const cow = `<pre>
          (__)
@@ -32,115 +33,91 @@ bot.text(({ message, reply }) => {
 	if(text == undefined)
 		reply("unknown error");
 */
-bot.command("start", ({ message, reply }) => {
-	if(args(message.text).includes("moo"))
-		reply(cow, { parse_mode: "HTML" });
-	reply("fuck off");
-});
+bot.command("start", ({ reply }) =>
+	reply("fuck off"));
 
-bot.command("price", ({ message, reply }) => {
-	if(args(message.text).includes("moo"))
-		reply(cow, { parse_mode: "HTML" });
-	const coin = String(args(message.text));
+bot.command("price", ({ message, reply }) =>
 	json("https://api.coinmarketcap.com/v1/ticker/")
 		.then(crap => {
-			const balls = crap.find(obj => obj.symbol === coin.toUpperCase());
+			const balls = crap.find(obj => obj.symbol === String(args(message.text)).toUpperCase());
 			if(balls == undefined)
 				reply("give me a valid symbol retard");
 			reply(balls.name + ": " + balls.price_usd + "$");
-		});
-});
+		}));
 
 bot.command("weather", ({ message, reply }) => {
-	if(args(message.text).includes("moo"))
-		reply(cow, { parse_mode: "HTML" });
 	const K = 273.15;
 	const city = args(message.text);
 	const link = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=1566ed87c9944f0df94332da29ee817c`;
 	let icon;
-	json(link)
-		.then(data => {
-			switch(data.weather[0].icon) {
-			case "01d":
-				icon = "☀";
-				break;
-			case "01n":
-				icon = "🌕";
-				break;
-			case "02d":
-			case "02n":
-				icon = "🌤";
-				break;
-			case "03d":
-			case "03n":
-				icon = "⛅";
-				break;
-			case "04d":
-			case "04n":
-				icon = "☁";
-				break;
-			case "09d":
-			case "09n":
-				icon = "🌧";
-				break;
-			case "10d":
-			case "10n":
-				icon = "🌦";
-				break;
-			case "11d":
-			case "11n":
-				icon = "🌩";
-				break;
-			case "13d":
-			case "13n":
-				icon = "🌨";
-				break;
-			case "50d":
-			case "50n":
-				icon = "🌫";
-				break;
-			}
-			reply(`Weather in ${data.name}, ${data.sys.country}: ${Math.floor(data.main.temp - K)}°C, ${data.weather[0].description} ` + icon + `
+	json(link).then(data => {
+		switch(data.weather[0].icon) {
+		case "01d":
+			icon = "☀";
+			break;
+		case "01n":
+			icon = "🌕";
+			break;
+		case "02d":
+		case "02n":
+			icon = "🌤";
+			break;
+		case "03d":
+		case "03n":
+			icon = "⛅";
+			break;
+		case "04d":
+		case "04n":
+			icon = "☁";
+			break;
+		case "09d":
+		case "09n":
+			icon = "🌧";
+			break;
+		case "10d":
+		case "10n":
+			icon = "🌦";
+			break;
+		case "11d":
+		case "11n":
+			icon = "🌩";
+			break;
+		case "13d":
+		case "13n":
+			icon = "🌨";
+			break;
+		case "50d":
+		case "50n":
+			icon = "🌫";
+			break;
+		}
+		reply(`Weather in ${data.name}, ${data.sys.country}: ${Math.floor(data.main.temp - K)}°C, ${data.weather[0].description} ` + icon + `
  Humidity: ${Math.floor(data.main.humidity)}%
  Air pressure: ${Math.floor(data.main.pressure)} hPa
 `);
-		});
+	});
 });
 
-bot.command("skelet", ({ message, reply }) => {
-	if(args(message.text).includes("moo"))
-		reply(cow, { parse_mode: "HTML" });
+bot.command("skelet", ({ reply }) => {
 	let skelets = "";
 	for (let i = 0; i < Math.floor((Math.random() * 20) + 1 ); i++)
 		skelets += Math.random() < 0.5 ? "💀" : "☠";
 	reply(skelets);
 });
 
-bot.command("cowsay", ({ message, reply }) => {
-	const moo = args(message.text);
-	if(moo == undefined || moo == "moo" || moo == "")
-		reply("```" + cowsay.say({
-			text : "Have you mooed today?"
-		}) + "```", { parse_mode: "Markdown" });
-	else
-		reply("```" + cowsay.say({
-			text : moo
-		}) + "```", { parse_mode: "Markdown" });
-});
+bot.command("cowsay", ({ message, reply }) =>
+	reply("```" + cowsay.say({
+		text : String(args(message.text)) || "Have you mooed today?"
+	}) + "```", { parse_mode: "Markdown" }));
 
-bot.command("papiez", ({ message, reply, replyWithVideo }) => {
-	if(args(message.text).includes("moo"))
-		reply(cow, { parse_mode: "HTML" });
-	replyWithVideo("https://vignette4.wikia.nocookie.net/nonsensopedia/images/c/cf/Patron.gif/revision/latest?cb=20130929184445");
-});
+bot.command("papiez", ({ replyWithVideo }) =>
+	replyWithVideo("https://vignette4.wikia.nocookie.net/nonsensopedia/images/c/cf/Patron.gif/revision/latest?cb=20130929184445"));
 
-bot.command("moo", ({ reply }) => {
-	reply(cow, { parse_mode: "HTML" });
-});
+bot.command("moo", ({ reply }) =>
+	reply(cow, { parse_mode: "HTML" }));
 
-bot.command("rogue", ({ reply }) => {
-	reply(feature);
-});
+bot.command("rogue", ({ reply }) =>
+	reply(feature));
 
 /*
 bot.all(({ message, reply }) => {
